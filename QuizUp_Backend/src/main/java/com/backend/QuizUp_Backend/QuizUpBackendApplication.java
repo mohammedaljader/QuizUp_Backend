73fee954407810529bea1d559eddb045d0c74ffa
Mohammed Aljader <461166@student.fontys.nl>
2022-09-16 23:57:17 +0200

    backend added

---
+++ QuizUp_Backend/src/main/java/com/backend/QuizUp_Backend/QuizUpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.QuizUp_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizUpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizUpBackendApplication.class, args);
+	}
+
+}
---
+++ QuizUp_Backend/src/test/java/com/backend/QuizUp_Backend/QuizUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.QuizUp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizUpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
