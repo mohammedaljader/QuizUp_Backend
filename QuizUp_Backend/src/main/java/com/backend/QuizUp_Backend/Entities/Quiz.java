64373993e1676b19fb2c40db7fa7fbc54a902cc8
Mohammed Aljader <461166@student.fontys.nl>
2022-09-17 23:55:32 +0200

    failed to connect to mongodb

--- QuizUp_Backend/src/main/java/com/backend/QuizUp_Backend/Entities/Quiz.java
+++ QuizUp_Backend/src/main/java/com/backend/QuizUp_Backend/Entities/Quiz.java
@@ -1,2 +1,14 @@
-package com.backend.QuizUp_Backend.Entities;public class Quiz {
+package com.backend.QuizUp_Backend.Entities;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Quiz {
+    @Id
+    String quiz_id;
+    String quiz_question;
+    String quiz_answer;
 }
--- QuizUp_Backend/src/main/java/com/backend/QuizUp_Backend/Repository/QuizRepository.java
+++ QuizUp_Backend/src/main/java/com/backend/QuizUp_Backend/Repository/QuizRepository.java
@@ -1,2 +1,12 @@
-package com.backend.QuizUp_Backend.Repository;public interface QuizRepository {
+package com.backend.QuizUp_Backend.Repository;
+
+import com.backend.QuizUp_Backend.Entities.Quiz;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizRepository
+        extends MongoRepository<Quiz, String> {
+
+//    Quiz findByQuiz_id(String id);
 }
